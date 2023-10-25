16a36b26ac75ef15085835e609882a26fb63f518
chris wiggins <chrishwiggins@users.noreply.github.com>
2023-10-25 18:43:22 -0400

    eits

--- doit.sh
+++ doit.sh
@@ -1,5 +1,4 @@
-gh repo clone chrishwiggins/DRL-for-microgrid-energy-management
-py3 A3C_plusplus.py --train
-cd DRL-for-microgrid-energy-management/
+#gh repo clone chrishwiggins/DRL-for-microgrid-energy-management
+#cd DRL-for-microgrid-energy-management/
 py3 A3C_plusplus.py --train
 py3 A3C_plusplus.py --test
